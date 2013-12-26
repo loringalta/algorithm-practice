fe233c8febe2fbe17c8c34e05587497019cb8713
jcma <jcma@ubuntu.(none)>
2013-12-26 00:46:02 -0500

    cleaned up

--- anagrams.c
+++ anagrams.c
@@ -7,22 +7,28 @@ void anag(char string[], char string2[]){
 	int len2 = strlen(string2);
 	int i, j = 0;
 	char tmp;
-	printf("%s\n", string); 
 	while(i < len2){
+		printf("%s\n", string);		
 		if (string[i] == string2[i]){
 			i++;
 		}
+
 		for (j = i; j < len1 -1; j++){
 			tmp = string[j];
 			string[j] = string[j+1];
 			string[j+1] = tmp;
-			printf("%s\n", string);		}
+		}
+
 	}
+
 }
 
 int main(){
-	char str[] = "EVIL";
-	char str2[] = "LIVE";
+	char str[] = "PANAMA";
+	char str2[] = "ANAPMA";
 	anag(str, str2);
+	char str3[] = "GUM";
+	char str4[] = "MUG";
+	anag(str3, str4);
 	return 0;
 }
